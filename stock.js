543a862efcdfab91a1ed5e6a8865677519b7a50d
TataQuispe <152334280+TataQuispe@users.noreply.github.com>
2023-12-06 06:24:11 -0400

    Initial commit

---
+++ stock.js
@@ -0,0 +1,6 @@
+let stockProductos = [
+    {id: 1, nombre: "Revista 01/12/2023", tipo: "revista", cantidad: 1, desc: "ANUEL TENDRÁ UN HIJO"    ,precio: 16, },
+    {id: 2, nombre: "Revista 24/11/2023", tipo: "revista", cantidad: 1, desc: "MALUMA SE CASA"          ,precio: 14, },
+    {id: 3, nombre: "Revista 17/11/2023", tipo: "revista", cantidad: 1, desc: "SHAKIRA VA PRESA"        ,precio: 14, },
+    {id: 4, nombre: "Revista 10/11/2023", tipo: "revista", cantidad: 1, desc: "¿BAD BUNNY GAY?"         ,precio: 12, },
+   ]
